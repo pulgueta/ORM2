21216a1e2fe4513ae68c399de9a791dbc36fc43e
pulgueta_ <roariasaf@gmail.com>
2021-10-14 21:23:19 -0500

    a

--- ORMPrac2/Model/DB2Entity.edmx.sql
+++ ORMPrac2/Model/DB2Entity.edmx.sql
@@ -32,7 +32,7 @@ CREATE TABLE [dbo].[AGENTS] (
     [AGENT_CODE] int  NOT NULL,
     [AGENT_NAME] nvarchar(max)  NOT NULL,
     [WORKING_AREA] nvarchar(max)  NOT NULL,
-    [COMISSION] decimal(18,0)  NOT NULL,
+    [COMISSION] decimal(18,3)  NOT NULL,
     [PHONE_NO] nvarchar(max)  NOT NULL,
     [COUNTRY] nvarchar(max)  NOT NULL
 );
@@ -46,10 +46,10 @@ CREATE TABLE [dbo].[CUSTOMERS] (
     [WORKING_AREA] nvarchar(max)  NOT NULL,
     [CUST_COUNTRY] nvarchar(max)  NOT NULL,
     [GRADE] int  NOT NULL,
-    [OPENING_AMT] decimal(18,0)  NOT NULL,
-    [RECEIVE_AMT] decimal(18,0)  NOT NULL,
-    [PAYMENT_AMT] decimal(18,0)  NOT NULL,
-    [OUTSTANDING_AMT] decimal(18,0)  NOT NULL,
+    [OPENING_AMT] decimal(18,3)  NOT NULL,
+    [RECEIVE_AMT] decimal(18,3)  NOT NULL,
+    [PAYMENT_AMT] decimal(18,3)  NOT NULL,
+    [OUTSTANDING_AMT] decimal(18,3)  NOT NULL,
     [PHONE_NO] nvarchar(max)  NOT NULL,
     [AGENTS_CODE] int  NOT NULL
 );
@@ -58,8 +58,8 @@ GO
 -- Creating table 'ODER_NUM'
 CREATE TABLE [dbo].[ODER_NUM] (
     [ORD_NUM] int IDENTITY(1,1) NOT NULL,
-    [ORD_AMOUNT] decimal(18,0)  NOT NULL,
-    [ADVANCE_AMOUNT] decimal(18,0)  NOT NULL,
+    [ORD_AMOUNT] decimal(18,3)  NOT NULL,
+    [ADVANCE_AMOUNT] decimal(18,3)  NOT NULL,
     [ORD_DATE] datetime  NOT NULL,
     [ORD_DESCRIPTION] nvarchar(max)  NOT NULL,
     [AGENT_CODE] int  NOT NULL,
